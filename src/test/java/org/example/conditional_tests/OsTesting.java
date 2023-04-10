4a312a40bcc148e8d627e09821993cad87c5f92e
Vpp2000 <victor.ponce.p@uni.pe>
2023-04-10 17:31:44 -0500

    managing DI in two different ways

--- src/test/java/org/example/conditional_tests/OsTesting.java
+++ src/test/java/org/example/conditional_tests/OsTesting.java
@@ -1,12 +1,25 @@
 package org.example.conditional_tests;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OsTesting {
+    private TestInfo testInfo;
+    private TestReporter testReporter;
+
+
+    @BeforeEach
+    void setup(TestInfo testInfo, TestReporter testReporter) {
+        this.testInfo = testInfo;
+        this.testReporter = testReporter;
+    }
 
     @Test
     @EnabledOnOs(OS.WINDOWS)
@@ -16,7 +29,9 @@ public class OsTesting {
 
     @Test
     @EnabledOnOs(OS.LINUX)
+    @DisplayName("Testing for linux")
     void test_for_linux(){
+        testReporter.publishEntry("executing " + testInfo.getDisplayName() + " on method " + testInfo.getTestMethod());
         assertTrue(true);
     }
 }
--- src/test/java/org/example/repeated_tests/RepeatedTesting.java
+++ src/test/java/org/example/repeated_tests/RepeatedTesting.java
@@ -1,23 +1,28 @@
 package org.example.repeated_tests;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Tag("repeat")
 public class RepeatedTesting {
+
     @Test
     @DisplayName("Test without repetition")
-    void test_without_repetition(){
+    void test_without_repetition(TestInfo testInfo){
+        System.out.println("Executing: " + testInfo.getDisplayName() + " " + testInfo.getTestMethod() + "||| Tags: ->  " + testInfo.getTags());
         assertTrue(true);
     }
 
     @RepeatedTest(value = 3, name = "{displayName} --> Repetition n° {currentRepetition} of {totalRepetitions}")
-    @DisplayName("Test with repetition")
-    void test_with_repetition(RepetitionInfo info){
-        System.out.println("Current repetition: " + info.getCurrentRepetition());
+    void test_with_repetition(RepetitionInfo repetitionInfo, TestInfo testInfo){
+        System.out.println("Current repetition: " + repetitionInfo.getCurrentRepetition() + " display or method: " + testInfo.getTestMethod().orElse(null).getName());
         assertTrue(true);
     }
 }
